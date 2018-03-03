5593e6ba3e438ce5580309b01b8cc7704bc4f054
Emrys <emrys.r@gmail.com>
2018-03-03 00:59:58 +0000

    added json content type to challange 01 data source

--- 01/lastvalue.php
+++ 01/lastvalue.php
@@ -1,5 +1,9 @@
 <?php
+//CHALLENGE 01
+header('Content-Type: application/json');
+
 /**
  * get the feed as plain text and output it without any modification
  */
+
 echo file_get_contents('http://emoncms.org/feed/aget.json?id=166913');
